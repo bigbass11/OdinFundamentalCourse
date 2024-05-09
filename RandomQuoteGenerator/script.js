5dcb333eac31ba8567637178a3e89774cb64757d
bigbass11 <92268911+bigbass11@users.noreply.github.com>
2024-05-08 21:11:45 -0600

    add duplicate quote generated checker

--- RandomQuoteGenerator/script.js
+++ RandomQuoteGenerator/script.js
@@ -12,15 +12,21 @@ const quotes = [{
     person: "A$AP Rocky"
 },{
     quote: "Got so many chains, they call me Chaining Tatum.",
-    person: "A$AP Rocky"
+    person: "Drake"
 },{
     quote: "She got a big booty, so I call her Big Booty.",
     person: "2 Chainz"
 }]
 
-btn.addEventListener('click', function(){
-    let random = Math.floor(Math.random() * quotes.length);
+let random = 0;
+let previousRandom;
 
+btn.addEventListener('click', function(){
+    do {
+        previousRandom = random;
+        random = Math.floor(Math.random() * quotes.length);
+    } while (random == previousRandom);
+    console.log(random);
     quote.innerText = quotes[random].quote;
     person.innerText = quotes[random].person;
 })
